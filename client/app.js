faecc569209df0a470ce9c99bd4efab821a2391d
Jim <jimmybnguyen@yahoo.com>
2020-02-10 13:13:11 -0800

    got some divs going

---
+++ client/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log("please work");
+});
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+axios.get("https://www.oregonlive.com/portland/").then(urlResponse => {
+  let $ = cheerio.load(urlResponse.data);
+
+  $("h2.river-item__headline--small-screens").each((i, element) => {
+    const link = $(element)
+      .find("a.river-item__headline-link")
+      .attr("href")
+      .split(", ");
+
+    console.log(link);
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const connection = mongoose.connection;
 const PORT = process.env.PORT || 8000;
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/MongoMongoose";
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+connection.on("error", console.error.bind(console, "connection error:"));
+connection.once("open", function() {
+  console.log("connected to db instance");
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,5 +27,5 @@ const apiRoutes = require("./routes/apiRoutes");
 app.use("/", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`listening on local: http://localhost:${PORT}`);
+  console.log(`listening on local: http://localhost:${PORT}`);
 });
